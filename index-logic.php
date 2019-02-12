9e20e806c651406a200d4aab9f2a0a4a73301a73
Daniel McCullough <kisa7081@gmail.com>
2019-02-11 19:34:41 -0600

    Files for project 1

---
+++ index-logic.php
@@ -0,0 +1,22 @@
+<?php
+
+$name='Daniel McCullough';
+
+$about_me = 'I\'v been a software developer for just over twenty years now, and have decided to go back to school here at the Harvard extension school';
+
+$author = 'Mark Twain';
+
+$quotes = [
+    'The secret of getting ahead is getting started.',
+    'Continuous improvement is better than delayed perfection.',
+    'With courage you will dare to take risks, have the strength to be compassionate, and the wisdom to be humble. Courage is the foundation of integrity.',
+    'Life is short, break the rules. Forgive quickly, kiss slowly. Love truly. Laugh uncontrollably and never regret anything that makes you smile.',
+    'All you need in this life is ignorance and confidence, and then success is sure.',
+    'Give every day the chance to become the most beautiful of your life.',
+    'The world owes you nothing. It was here first.',
+    'I make it a rule never to smoke while I’m sleeping.',
+    'Let us endeavor so to live so that when we come to die even the undertaker will be sorry.',
+    'Forgiveness is the fragrance that the violet sheds on the heel that has crushed it.'
+];
+
+$quote = $quotes[rand(0, 9)];
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php require('index-logic.php'); ?>
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <title>Project 1</title>
+    <meta charset="urf-8" />
+    <link rel='stylesheet' href="css/simple.css" />
+</head>
+
+<body>
+    <h1><?=$name; ?></h1>
+    <img src="images/me.jpg" />
+    <h1>About me</h1>
+    <p>
+       <?=$about_me; ?>
+    </p>
+    <h1>Random quote from <?=$author; ?></h1>
+    <blockquote>
+        <?=$quote; ?>
+    </blockquote>
+</body>
+
+</html>
